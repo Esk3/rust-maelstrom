582942bd8d472599a6efa42519c3040fea800e5a
eske <eske.brun3@gmail.com>
2024-08-12 08:11:47 +0200

    init

---
+++ devenv.nix
@@ -0,0 +1,38 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ bacon jdk graphviz gnuplot ];
+
+  # https://devenv.sh/scripts/
+  scripts.maelstrom.exec = "cargo build && ./maelstrom/maelstrom $@";
+  scripts.maelstrom-no-build.exec = "./maelstrom/maelstrom $@";
+
+  enterShell = ''
+    fish
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.rust.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
