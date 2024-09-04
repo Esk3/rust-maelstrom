bb4edddbe0ef6b0d46588907630e900e0dbb22c1
eske <eske.brun3@gmail.com>
2024-09-04 18:48:28 +0200

    init

--- src/server.rs
+++ src/server.rs
@@ -5,23 +5,30 @@ use std::{
 };
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use tokio::io::AsyncBufReadExt;
 
-use crate::{message::Message, service::Service, Fut};
+use crate::{
+    message::{InitRequest, Message},
+    service::Service,
+    Fut, Node,
+};
 
 pub struct Server<H> {
     handler: H,
 }
 
 impl<H> Server<H> {
-    pub async fn run<T, Res>(mut self)
+    pub async fn run<T, Res, N>(mut self)
     where
-        H: crate::service::Service<Message<T>, Response = HandlerResponse<Message<Res>, T>>
+        H: crate::service::Service<HandlerInput<T, N>, Response = HandlerResponse<Message<Res>, T>>
             + Send
             + 'static,
-        T: Serialize + DeserializeOwned + Send + 'static + Debug,
+        T: Serialize + DeserializeOwned + Send + 'static + Debug + Clone,
         Res: Serialize + Send + 'static,
+        N: Node + Send + 'static,
     {
-        Self::init();
+        let node = Self::init().await;
+        let node = std::sync::Arc::new(std::sync::Mutex::new(node));
         let event_broker = EventBroker::new();
 
         let input = std::io::stdin().lines().next().unwrap().unwrap();
@@ -29,6 +36,12 @@ impl<H> Server<H> {
         let input = serde_json::from_str::<Message<T>>(&input).unwrap();
 
         let mut set = tokio::task::JoinSet::new();
+
+        let input = HandlerInput {
+            message: input,
+            node: node.clone(),
+            event_broker: event_broker.clone(),
+        };
 
         set.spawn(async move { self.handler.call(input).await });
 
@@ -38,7 +51,35 @@ impl<H> Server<H> {
             HandlerResponse::Event(event) => event_broker.publish_event(event),
         }
     }
-    fn init() {}
+    async fn init<N>() -> N
+    where
+        N: Node,
+    {
+        let stdin = tokio::io::stdin();
+        let mut lines = tokio::io::BufReader::new(stdin).lines();
+
+        let init_line = lines.next_line().await.unwrap().unwrap();
+        let init_message: Message<InitRequest> = serde_json::from_str(&init_line).unwrap();
+        let (reply, body) = init_message.into_reply();
+        let InitRequest::Init {
+            msg_id,
+            node_id,
+            node_ids,
+        } = body;
+
+        let node = N::init(node_id, node_ids);
+
+        {
+            let mut output = std::io::stdout().lock();
+            reply
+                .with_body(dbg!(crate::message::InitResponse::InitOk {
+                    in_reply_to: msg_id,
+                }))
+                .send(&mut output);
+            std::io::Write::flush(&mut output).unwrap();
+        }
+        node
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -85,22 +126,29 @@ where
     }
     pub fn subscribe(&self, id: usize) -> tokio::sync::oneshot::Receiver<Message<T>> {
         let (tx, rx) = tokio::sync::oneshot::channel();
-        self.new_ids_tx.send((id, tx));
+        self.new_ids_tx.send((id, tx)).unwrap();
         rx
     }
     pub fn publish_event(&self, event: Event<T>) {
-        self.events_tx.send(event);
+        self.events_tx.send(event).unwrap();
     }
 }
 
 struct TestHandler;
-impl Service<Message<MyT>> for TestHandler {
+impl Service<HandlerInput<MyT, MyNode>> for TestHandler {
     type Response = HandlerResponse<Message<()>, MyT>;
 
     type Future = Fut<Self::Response>;
 
-    fn call(&mut self, request: Message<MyT>) -> Self::Future {
-        let (msg, body) = request.split();
+    fn call(
+        &mut self,
+        HandlerInput {
+            message,
+            node,
+            event_broker,
+        }: HandlerInput<MyT, MyNode>,
+    ) -> Self::Future {
+        let (msg, body) = message.split();
         match body {
             MyT::Echo { echo } => todo!(),
             MyT::Broadcast {} => todo!(),
@@ -112,7 +160,13 @@ impl Service<Message<MyT>> for TestHandler {
         todo!()
     }
 }
-#[derive(Debug, Serialize, Deserialize)]
+struct MyNode;
+impl Node for MyNode {
+    fn init(node_id: String, node_ids: Vec<String>) -> Self {
+        todo!()
+    }
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(untagged)]
 enum MyT {
     Echo { echo: String },
@@ -124,4 +178,9 @@ async fn test() {
         handler: TestHandler,
     };
     s.run().await;
+}
+pub struct HandlerInput<T, N> {
+    message: Message<T>,
+    node: Arc<Mutex<N>>,
+    event_broker: EventBroker<T>,
 }
